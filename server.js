979f4b2e320fa5d5fe05174fcca24c089e8a6eb0
adithya-pixel <adithyasiju9@gmail.com>
2024-11-07 22:24:30 -0800

    Initial commit for portfolio-Express project

---
+++ server.js
@@ -0,0 +1,13 @@
+var express=require('express');
+var app=express();
+app.use(express.json());
+app.use(express.urlencoded({
+    extended:true
+}));
+app.use(express.static('public'));
+//app.use(express.static('public/CSS'));
+//app.use(express.static('public/IMG'));
+app.set('view engine','ejs');
+app.set('views','./views');
+app.get('/',function(req,res){res.render('index');});
+app.listen(8000);
